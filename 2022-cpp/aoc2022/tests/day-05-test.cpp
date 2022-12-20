badf9e940ca7eda3179c6d96c4569dfcad4f8209
megan <shadowradiance@me.com>
2022-12-20 04:14:19 -0400

    Day 5 Complete

--- 2022-cpp/aoc2022/tests/day-05-test.cpp
+++ 2022-cpp/aoc2022/tests/day-05-test.cpp
@@ -5,15 +5,33 @@
 TEST(Day05, Part1Example)
 {
   std::vector<std::string> data{
+    "    [D]    ",
+    "[N] [C]    ",
+    "[Z] [M] [P]",
+    " 1   2   3 ",
+    " ",
+    "move 1 from 2 to 1",
+    "move 3 from 1 to 3",
+    "move 2 from 2 to 1",
+    "move 1 from 1 to 2",
   };
 
-  EXPECT_EQ("PENDING", day_05::answer_a(data));
+  EXPECT_EQ("CMZ", day_05::answer_a(data));
 }
 
 TEST(Day05, Part2Example)
 {
   std::vector<std::string> data{
+    "    [D]    ",
+    "[N] [C]    ",
+    "[Z] [M] [P]",
+    " 1   2   3 ",
+    " ",
+    "move 1 from 2 to 1",
+    "move 3 from 1 to 3",
+    "move 2 from 2 to 1",
+    "move 1 from 1 to 2",
   };
 
-  EXPECT_EQ("PENDING", day_05::answer_b(data));
+  EXPECT_EQ("MCD", day_05::answer_b(data));
 }
