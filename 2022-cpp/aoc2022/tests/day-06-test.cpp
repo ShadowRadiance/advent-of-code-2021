ecdb958dbc78280fae25f6bba05b03d125d7cc56
megan <shadowradiance@me.com>
2022-12-23 00:46:51 -0400

    Day 6 Complete

--- 2022-cpp/aoc2022/tests/day-06-test.cpp
+++ 2022-cpp/aoc2022/tests/day-06-test.cpp
@@ -2,18 +2,93 @@
 
 #include <days.h>
 
-TEST(Day06, Part1Example)
+TEST(Day06, Part1Example1)
 {
   std::vector<std::string> data{
+    "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
   };
 
-  EXPECT_EQ("PENDING", day_06::answer_a(data));
+  EXPECT_EQ("7", day_06::answer_a(data));
 }
 
-TEST(Day06, Part2Example)
+TEST(Day06, Part1Example2)
 {
   std::vector<std::string> data{
+    "bvwbjplbgvbhsrlpgdmjqwftvncz",
   };
 
-  EXPECT_EQ("PENDING", day_06::answer_b(data));
+  EXPECT_EQ("5", day_06::answer_a(data));
 }
+
+TEST(Day06, Part1Example3)
+{
+  std::vector<std::string> data{
+    "nppdvjthqldpwncqszvftbrmjlhg",
+  };
+
+  EXPECT_EQ("6", day_06::answer_a(data));
+}
+
+TEST(Day06, Part1Example4)
+{
+  std::vector<std::string> data{
+    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+  };
+
+  EXPECT_EQ("10", day_06::answer_a(data));
+}
+
+TEST(Day06, Part1Example5)
+{
+  std::vector<std::string> data{
+    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+  };
+
+  EXPECT_EQ("11", day_06::answer_a(data));
+}
+
+TEST(Day06, Part2Example1)
+{
+  std::vector<std::string> data{
+    "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+  };
+
+  EXPECT_EQ("19", day_06::answer_b(data));
+}
+
+TEST(Day06, Part2Example2)
+{
+  std::vector<std::string> data{
+    "bvwbjplbgvbhsrlpgdmjqwftvncz",
+  };
+
+  EXPECT_EQ("23", day_06::answer_b(data));
+}
+
+TEST(Day06, Part2Example3)
+{
+  std::vector<std::string> data{
+    "nppdvjthqldpwncqszvftbrmjlhg",
+  };
+
+  EXPECT_EQ("23", day_06::answer_b(data));
+}
+
+TEST(Day06, Part2Example4)
+{
+  std::vector<std::string> data{
+    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+  };
+
+  EXPECT_EQ("29", day_06::answer_b(data));
+}
+
+TEST(Day06, Part2Example5)
+{
+  std::vector<std::string> data{
+    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+  };
+
+  EXPECT_EQ("26", day_06::answer_b(data));
+}
+
