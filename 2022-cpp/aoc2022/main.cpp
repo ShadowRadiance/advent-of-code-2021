0f80eb9cc62c425614eccd1b872e80ea63517d12
megan <shadowradiance@me.com>
2022-12-28 22:33:01 -0400

    Add Timings and make main a bit nicer.

--- 2022-cpp/aoc2022/main.cpp
+++ 2022-cpp/aoc2022/main.cpp
@@ -1,60 +1,89 @@
 #include <iostream>
+#include <chrono>
 
 #include <days.h>
 #include <utility.h>
+
+
+template<typename Fn>
+std::tuple<std::string, std::chrono::microseconds> time(Fn fn, const std::vector<std::string>& data) {
+    auto start = std::chrono::high_resolution_clock::now();
+    auto result = fn(data);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = duration_cast<std::chrono::microseconds>(stop - start);
+    return { result, duration };
+}
+
+std::string path(int n) {
+    if (n < 10) return std::string{ "./data/day-0" + std::to_string(n) + ".txt" };
+    return std::string{ "./data/day-" + std::to_string(n) + ".txt" };
+}
+
+using AnswerFn = std::string(*)(const std::vector<std::string>&);
+auto methods = std::vector<AnswerFn>{
+    day_01::answer_a,
+    day_01::answer_b,
+    day_02::answer_a,
+    day_02::answer_b,
+    day_03::answer_a,
+    day_03::answer_b,
+    day_04::answer_a,
+    day_04::answer_b,
+    day_05::answer_a,
+    day_05::answer_b,
+    day_06::answer_a,
+    day_06::answer_b,
+    day_07::answer_a,
+    day_07::answer_b,
+    day_08::answer_a,
+    day_08::answer_b,
+    day_09::answer_a,
+    day_09::answer_b,
+    day_10::answer_a,
+    day_10::answer_b,
+    day_11::answer_a,
+    day_11::answer_b,
+    day_12::answer_a,
+    day_12::answer_b,
+    day_13::answer_a,
+    day_13::answer_b,
+    day_14::answer_a,
+    day_14::answer_b,
+    day_15::answer_a,
+    day_15::answer_b,
+    day_16::answer_a,
+    day_16::answer_b,
+    day_17::answer_a,
+    day_17::answer_b,
+    day_18::answer_a,
+    day_18::answer_b,
+    day_19::answer_a,
+    day_19::answer_b,
+    day_20::answer_a,
+    day_20::answer_b,
+    day_21::answer_a,
+    day_21::answer_b,
+    day_22::answer_a,
+    day_22::answer_b,
+    day_23::answer_a,
+    day_23::answer_b,
+    day_24::answer_a,
+    day_24::answer_b,
+    day_25::answer_a,
+    day_25::answer_b,
+};
 
 int main(int argc, char** argv)
 {
     using std::cout;
 
-    cout << "Day 01 Answer A: " << day_01::answer_a(load_data("./data/day-01.txt")) << "\n";
-    cout << "Day 01 Answer B: " << day_01::answer_b(load_data("./data/day-01.txt")) << "\n";
-    cout << "Day 02 Answer A: " << day_02::answer_a(load_data("./data/day-02.txt")) << "\n";
-    cout << "Day 02 Answer B: " << day_02::answer_b(load_data("./data/day-02.txt")) << "\n";
-    cout << "Day 03 Answer A: " << day_03::answer_a(load_data("./data/day-03.txt")) << "\n";
-    cout << "Day 03 Answer B: " << day_03::answer_b(load_data("./data/day-03.txt")) << "\n";
-    cout << "Day 04 Answer A: " << day_04::answer_a(load_data("./data/day-04.txt")) << "\n";
-    cout << "Day 04 Answer B: " << day_04::answer_b(load_data("./data/day-04.txt")) << "\n";
-    cout << "Day 05 Answer A: " << day_05::answer_a(load_data("./data/day-05.txt")) << "\n";
-    cout << "Day 05 Answer B: " << day_05::answer_b(load_data("./data/day-05.txt")) << "\n";
-    cout << "Day 06 Answer A: " << day_06::answer_a(load_data("./data/day-06.txt")) << "\n";
-    cout << "Day 06 Answer B: " << day_06::answer_b(load_data("./data/day-06.txt")) << "\n";
-    cout << "Day 07 Answer A: " << day_07::answer_a(load_data("./data/day-07.txt")) << "\n";
-    cout << "Day 07 Answer B: " << day_07::answer_b(load_data("./data/day-07.txt")) << "\n";
-    cout << "Day 08 Answer A: " << day_08::answer_a(load_data("./data/day-08.txt")) << "\n";
-    cout << "Day 08 Answer B: " << day_08::answer_b(load_data("./data/day-08.txt")) << "\n";
-    cout << "Day 09 Answer A: " << day_09::answer_a(load_data("./data/day-09.txt")) << "\n";
-    cout << "Day 09 Answer B: " << day_09::answer_b(load_data("./data/day-09.txt")) << "\n";
-    cout << "Day 10 Answer A: " << day_10::answer_a(load_data("./data/day-10.txt")) << "\n";
-    cout << "Day 10 Answer B: " << day_10::answer_b(load_data("./data/day-10.txt")) << "\n";
-    cout << "Day 11 Answer A: " << day_11::answer_a(load_data("./data/day-11.txt")) << "\n";
-    cout << "Day 11 Answer B: " << day_11::answer_b(load_data("./data/day-11.txt")) << "\n";
-    cout << "Day 12 Answer A: " << day_12::answer_a(load_data("./data/day-12.txt")) << "\n";
-    cout << "Day 12 Answer B: " << day_12::answer_b(load_data("./data/day-12.txt")) << "\n";
-    cout << "Day 13 Answer A: " << day_13::answer_a(load_data("./data/day-13.txt")) << "\n";
-    cout << "Day 13 Answer B: " << day_13::answer_b(load_data("./data/day-13.txt")) << "\n";
-    cout << "Day 14 Answer A: " << day_14::answer_a(load_data("./data/day-14.txt")) << "\n";
-    cout << "Day 14 Answer B: " << day_14::answer_b(load_data("./data/day-14.txt")) << "\n";
-    cout << "Day 15 Answer A: " << day_15::answer_a(load_data("./data/day-15.txt")) << "\n";
-    cout << "Day 15 Answer B: " << day_15::answer_b(load_data("./data/day-15.txt")) << "\n";
-    cout << "Day 16 Answer A: " << day_16::answer_a(load_data("./data/day-16.txt")) << "\n";
-    cout << "Day 16 Answer B: " << day_16::answer_b(load_data("./data/day-16.txt")) << "\n";
-    cout << "Day 17 Answer A: " << day_17::answer_a(load_data("./data/day-17.txt")) << "\n";
-    cout << "Day 17 Answer B: " << day_17::answer_b(load_data("./data/day-17.txt")) << "\n";
-    cout << "Day 18 Answer A: " << day_18::answer_a(load_data("./data/day-18.txt")) << "\n";
-    cout << "Day 18 Answer B: " << day_18::answer_b(load_data("./data/day-18.txt")) << "\n";
-    cout << "Day 19 Answer A: " << day_19::answer_a(load_data("./data/day-19.txt")) << "\n";
-    cout << "Day 19 Answer B: " << day_19::answer_b(load_data("./data/day-19.txt")) << "\n";
-    cout << "Day 20 Answer A: " << day_20::answer_a(load_data("./data/day-20.txt")) << "\n";
-    cout << "Day 20 Answer B: " << day_20::answer_b(load_data("./data/day-20.txt")) << "\n";
-    cout << "Day 21 Answer A: " << day_21::answer_a(load_data("./data/day-21.txt")) << "\n";
-    cout << "Day 21 Answer B: " << day_21::answer_b(load_data("./data/day-21.txt")) << "\n";
-    cout << "Day 22 Answer A: " << day_22::answer_a(load_data("./data/day-22.txt")) << "\n";
-    cout << "Day 22 Answer B: " << day_22::answer_b(load_data("./data/day-22.txt")) << "\n";
-    cout << "Day 23 Answer A: " << day_23::answer_a(load_data("./data/day-23.txt")) << "\n";
-    cout << "Day 23 Answer B: " << day_23::answer_b(load_data("./data/day-23.txt")) << "\n";
-    cout << "Day 24 Answer A: " << day_24::answer_a(load_data("./data/day-24.txt")) << "\n";
-    cout << "Day 24 Answer B: " << day_24::answer_b(load_data("./data/day-24.txt")) << "\n";
-    cout << "Day 25 Answer A: " << day_25::answer_a(load_data("./data/day-25.txt")) << "\n";
-    cout << "Day 25 Answer B: " << day_25::answer_b(load_data("./data/day-25.txt")) << "\n";
+    for (int i = 0; i <= 49; i++) {
+        auto day = 1 + (i / 2);
+        auto data = load_data(path(day));
+        auto fn = methods[i];
+        auto [result, duration] = time(fn, data);
+        
+        cout << "Day " << ((day < 10) ? "0" : "") << day << " Answer " << ((i % 2 == 0) ? "A" : "B") << ": "
+             << "(" << std::setw(10) << duration << ") " << result << "\n";
+    }
 }
