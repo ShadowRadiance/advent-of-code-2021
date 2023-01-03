600fa40975c1a2c3d19e31236306bfa3336b4ce6
megan <shadowradiance@me.com>
2023-01-02 22:45:55 -0400

    Day 13 Part 1 (thanks to Sy Brand for tips)

--- 2022-cpp/aoc2022/src/day-13.cpp
+++ 2022-cpp/aoc2022/src/day-13.cpp
@@ -1,17 +1,164 @@
 #include <days.h>
+#include <variant>
+#include <optional>
+#include <utility>
+#include <memory>
+#include <sstream>
+#include <cassert>
+#include <numeric>
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+namespace Packet13
+{
+    using std::string;
+    using std::vector;
+    using std::variant;
+
+    template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+    //template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+    struct Node
+    {
+        variant<int, vector<Node>> data;
+
+        explicit Node(int i) : data(i) {}
+        explicit Node(vector<Node> v) : data(std::move(v)) {}
+
+        friend bool operator<(Node const& lhs, Node const& rhs)
+        {
+            auto overloads = overloaded{
+                [](int l, int r) { return l < r; },
+                [](vector<Node> const& l, vector<Node> const& r) { return l < r; },
+                [](int l, vector<Node> const& r) { return vector<Node>{Node{l}} < r; },
+                [](vector<Node> const& l, int r) { return l < vector<Node>{Node{r}}; },
+            };
+            return std::visit(overloads, lhs.data, rhs.data);
+        }
+
+        friend auto operator<<(std::ostream& os, Node& node) -> std::ostream& {
+            if (std::holds_alternative<vector<Node>>(node.data)) {
+                os << "[";
+                bool firstTime = true;
+                for (Node& node : std::get<vector<Node>>(node.data)) {
+                    if (!firstTime) os << ",";
+                    os << node;
+                    firstTime = false;                    
+                }
+                os << "]";
+            }
+            else {
+                os << std::get<int>(node.data);
+            }
+            return os;
+        }
+    };
+
+    int parseInt(auto& it, auto end)
+    {
+        size_t size{0};
+        int parsed = std::stoi(string{ it, end }, &size);
+        it += size;
+        return parsed;
+    }
+
+    vector<Node> parseList(auto& it, auto end)
+    {
+        assert(*it == '[');
+        ++it;
+
+        vector<Node> nodes;
+
+        while (it != end && *it != ']') {
+            if (std::isdigit(*it)) {
+                nodes.emplace_back(parseInt(it, end)); // parseInt will advance it
+            }
+            else if (*it == '[') {
+                nodes.emplace_back(parseList(it, end)); // recurse,  advancing it
+            }
+            if (*it == ',') ++it;
+        }
+
+
+        assert(*it == ']');
+        ++it;
+
+        return nodes;
+    }
+
+
+    Node parseNode(string s)
+    {
+        auto it = s.begin();
+        auto end = s.end();
+        vector<Node> children = parseList(it, end);
+        return Node{ children };
+    }
+}
 
 namespace day_13
 {
-  using std::string;
-  using std::vector;
+    using std::string;
+    using std::vector;
+    using std::pair;
+    
+    using Packet13::Node;
+    using Packet13::parseNode;
 
-  string answer_a(const vector<string>& input_data)
-  {
-    return "PENDING";
-  }
+    struct NodePair
+    {
+        size_t number = 0;
+        pair<Node, Node> pair;
+    };
+    using NodePairs = vector<NodePair>;
 
-  string answer_b(const vector<string>& input_data)
-  {
-    return "PENDING";
-  }
+    NodePairs parseInput(vector<string> const& input)
+    {
+        vector<string> filtered;
+        copy_if(input.begin(), input.end(), back_inserter(filtered), [](auto& s) { return !s.empty();  });
+
+        vector<Node> allNodes;
+        std::transform(filtered.begin(), filtered.end(), back_inserter(allNodes), parseNode);
+
+        NodePairs result;
+        auto it = allNodes.begin();
+        auto end = allNodes.end();
+        while (it != end) {
+            result.emplace_back(result.size()+1, std::make_pair(*it, *(it + 1)));
+            it += 2;
+        }
+
+        return result;
+    }
+
+    bool ordered(NodePair& nodePair)
+    {
+        return nodePair.pair.first < nodePair.pair.second;
+    }
+
+    string answer_a(const vector<string>& input_data)
+    {
+        NodePairs nodePairs = parseInput(input_data);
+        //std::cout << "Pairs 1, 2, 4, and 6 should be considered ordered correctly." << std::endl;
+
+        int sum = 0;
+        for (NodePair& nodePair : nodePairs)
+        {
+            //std::cout << "PACKET " << nodePair.number << "A:" << nodePair.pair.first << std::endl;
+            //std::cout << "PACKET " << nodePair.number << "B:" << nodePair.pair.second << std::endl;
+
+            if (ordered(nodePair)) {
+                //std::cout << "Pair " << nodePair.number << " is considered ordered." << std::endl;
+                sum += nodePair.number;
+            }
+            //std::cout << std::endl;
+        }
+        return std::to_string(sum);
+    }
+
+    string answer_b(const vector<string>& input_data)
+    {
+        return "PENDING";
+    }
 }
--- 2022-cpp/aoc2022/tests/day-13-test.cpp
+++ 2022-cpp/aoc2022/tests/day-13-test.cpp
@@ -2,18 +2,38 @@
 
 #include <days.h>
 
+std::vector<std::string> data = {
+    "[1,1,3,1,1]",
+    "[1,1,5,1,1]",
+    "",
+    "[[1],[2,3,4]]",
+    "[[1],4]",
+    "",
+    "[9]",
+    "[[8,7,6]]",
+    "",
+    "[[4,4],4,4]",
+    "[[4,4],4,4,4]",
+    "",
+    "[7,7,7,7]",
+    "[7,7,7]",
+    "",
+    "[]",
+    "[3]",
+    "",
+    "[[[]]]",
+    "[[]]",
+    "",
+    "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+    "[1,[2,[3,[4,[5,6,0]]]],8,9]",
+};
+
 TEST(Day13, Part1Example)
 {
-  std::vector<std::string> data{
-  };
-
-  EXPECT_EQ("PENDING", day_13::answer_a(data));
+    EXPECT_EQ("13", day_13::answer_a(data));
 }
 
 TEST(Day13, Part2Example)
 {
-  std::vector<std::string> data{
-  };
-
-  EXPECT_EQ("PENDING", day_13::answer_b(data));
+    EXPECT_EQ("PENDING", day_13::answer_b(data));
 }
