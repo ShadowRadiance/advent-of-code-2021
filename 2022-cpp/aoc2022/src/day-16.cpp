42571d2398e081bd3eddd8cad6c91f31f2be0761
Megan McVey <megan.mcvey@shopify.com>
2023-01-17 18:36:01 -0400

    Day 16 Start & Parsing

--- 2022-cpp/aoc2022/src/day-16.cpp
+++ 2022-cpp/aoc2022/src/day-16.cpp
@@ -1,14 +1,103 @@
 #include <days.h>
 
+#include <cassert>
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <optional>
+#include <sstream>
+#include <unordered_map>
+
 namespace day_16
 {
   using std::string;
   using std::vector;
 
-  string answer_a(const vector<string>& input_data)
-  {
-    return "PENDING";
-  }
+    struct Valve
+    {
+        string name{};
+        int flow{0};
+        vector<string> connectedValveNames{};
+    };
+
+    int parseInt(auto& it, auto end)
+    {
+        size_t size{0};
+        int parsed = std::stoi(string{it, end}, &size);
+        it += size;
+        return parsed;
+    }
+
+    Valve parseValve(const string& str)
+    {
+        auto it = str.begin();
+        auto end = str.end();
+
+        assert(*it == 'V');
+        it += 6;                 // Valve_
+        string name{it, it + 2}; // AA
+        it += 2;
+        assert(*it == ' ');
+        it += 15; // _has_flow_rate=
+        int flowRate = parseInt(it, end);
+        assert(*it == ';');
+        it += 23;             // ;_tunnels_lead_to_valve
+        if (*it == 's') ++it; // s
+        vector<string> connectedValveNames;
+        while (it != end) { // DD, II, BB
+            if (*it >= 'A' && *it <= 'Z') {
+                connectedValveNames.push_back({it, it + 2});
+                it += 2;
+            } else {
+                ++it;
+            }
+        }
+
+        return Valve{name, flowRate, connectedValveNames};
+    }
+
+    vector<Valve> parseValves(const vector<string>& data)
+    {
+        vector<Valve> valves;
+        std::transform(data.begin(),
+                       data.end(),
+                       back_inserter(valves),
+                       parseValve);
+        return valves;
+    }
+
+    vector<vector<int>> buildAdjancyMatrix(const vector<Valve>& valves) {
+        int size = valves.size();
+        vector<vector<int>> adjancyMatrix(size, vector<int>(size));
+        for (size_t i = 0; i < size; i++) {
+            Valve valve = valves[i];
+            for (string name : valve.connectedValveNames) {
+                auto it = std::find_if(
+                    valves.begin(),
+                    valves.end(),
+                    [&name](auto& _) { return _.name == name; });
+                if (it == valves.end())
+                    throw std::runtime_error("expected to find " + name +
+                                             " in valves!");
+                int indexOfConnectedValve = std::distance(valves.begin(), it);
+                adjancyMatrix[i][indexOfConnectedValve] = 1; // link forward
+                adjancyMatrix[indexOfConnectedValve][i] = 1; // link reverse
+            }
+        }
+        return adjancyMatrix;
+    }
+
+    int solve(const vector<Valve>& valves)
+    {
+        vector<vector<int>> adjancyMatrix = buildAdjancyMatrix(valves);
+
+        return 0;
+    }
+
+    string answer_a(const vector<string>& input_data)
+    {
+        return std::to_string(solve(parseValves(input_data)));
+    }
 
   string answer_b(const vector<string>& input_data)
   {
--- 2022-cpp/aoc2022/tests/day-16-test.cpp
+++ 2022-cpp/aoc2022/tests/day-16-test.cpp
@@ -2,18 +2,25 @@
 
 #include <days.h>
 
+const std::vector<std::string> data{
+  "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
+  "Valve BB has flow rate=13; tunnels lead to valves CC, AA",
+  "Valve CC has flow rate=2; tunnels lead to valves DD, BB",
+  "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE",
+  "Valve EE has flow rate=3; tunnels lead to valves FF, DD",
+  "Valve FF has flow rate=0; tunnels lead to valves EE, GG",
+  "Valve GG has flow rate=0; tunnels lead to valves FF, HH",
+  "Valve HH has flow rate=22; tunnel leads to valve GG",
+  "Valve II has flow rate=0; tunnels lead to valves AA, JJ",
+  "Valve JJ has flow rate=21; tunnel leads to valve II",
+};
+
 TEST(Day16, Part1Example)
 {
-  std::vector<std::string> data{
-  };
-
-  EXPECT_EQ("PENDING", day_16::answer_a(data));
+  EXPECT_EQ("1651", day_16::answer_a(data));
 }
 
 TEST(Day16, Part2Example)
 {
-  std::vector<std::string> data{
-  };
-
   EXPECT_EQ("PENDING", day_16::answer_b(data));
 }
