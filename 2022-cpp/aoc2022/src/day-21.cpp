6853efd3ef2608c8c9f76bfe0c9e0d459a9723a2
megan <shadowradiance@me.com>
2023-02-27 23:54:47 -0400

    Day 21 Part 1

--- 2022-cpp/aoc2022/src/day-21.cpp
+++ 2022-cpp/aoc2022/src/day-21.cpp
@@ -1,16 +1,128 @@
 #include <days.h>
+#include <memory>
+#include <algorithm>
+#include <iterator>
+#include <unordered_map>
 
 namespace day_21
 {
     using std::string;
     using std::vector;
+    using std::unordered_map;
+    using std::shared_ptr;
 
-    string answer_a(const vector<string>& input_data)
+    class Monkey;
+    using MonkeyMap = unordered_map<string, shared_ptr<Monkey>>;
+
+    class Monkey
     {
-        return "PENDING";
+    public:
+        Monkey(string name)
+            : name(name)
+        {}
+        virtual int64_t evaluate() const = 0;
+        virtual ~Monkey() = default;
+    private:
+        string name;
+    };
+
+    class NumberMonkey : public Monkey
+    {
+    public:
+        NumberMonkey(string name, int64_t number)
+            : Monkey(name), number(number)
+        {}
+        int64_t evaluate() const override
+        {
+            return number;
+        }
+    private:
+        int64_t number;
+    };
+
+    class OperationMonkey : public Monkey
+    {
+    public:
+        OperationMonkey(string name, string first, char op, string second)
+            : Monkey(name), first(first), op(op), second(second)
+        {}
+        int64_t evaluate() const override
+        {
+            switch (op) {
+            case '+': return firstMonkey->evaluate() + secondMonkey->evaluate();;
+            case '-': return firstMonkey->evaluate() - secondMonkey->evaluate();
+            case '*': return firstMonkey->evaluate() * secondMonkey->evaluate();
+            case '/': return firstMonkey->evaluate() / secondMonkey->evaluate();
+            default: return 0;
+            }
+        }
+        void connectSources(MonkeyMap const& monkeyMap)
+        {
+            firstMonkey = monkeyMap.at(first);
+            secondMonkey = monkeyMap.at(second);
+        }
+    private:
+        string first;
+        char op;
+        string second;
+        shared_ptr<Monkey> firstMonkey;
+        shared_ptr<Monkey> secondMonkey;
+    };
+
+    int64_t parseInt(auto& it, auto end)
+    {
+        size_t size{ 0 };
+        int parsed = std::stoll(string{ it, end }, &size);
+        it += size;
+        return parsed;
     }
 
-    string answer_b(const vector<string>& input_data)
+    MonkeyMap::value_type
+        parseMonkey(string const& str)
+    {
+        // name: first op last
+        // name: number
+
+        auto it = str.begin();
+        auto end = str.end();
+
+        string name(it, it + 4);
+        it += 6;
+
+        if (std::isdigit(*it)) {
+            int64_t number = parseInt(it, end);
+            return { name, std::make_shared<NumberMonkey>(name, number) };
+        } else {
+            string first(it, it + 4);
+            it += 5;
+            char op = *it;
+            it += 2;
+            string last(it, it + 4);
+            return { name, std::make_shared<OperationMonkey>(name, first, op, last) };
+        }
+    }
+
+    string answer_a(vector<string> const& inputData)
+    {
+        MonkeyMap monkeys;
+        std::transform(
+            inputData.begin(), inputData.end(),
+            std::inserter(monkeys, monkeys.end()),
+            parseMonkey
+        );
+        std::for_each(
+            monkeys.begin(), monkeys.end(),
+            [&monkeys](auto& pair) {
+                auto& [name, monkey] = pair;
+                auto opMonkey = dynamic_pointer_cast<OperationMonkey>(monkey);
+                if (opMonkey) opMonkey->connectSources(monkeys);
+            }
+        );
+
+        return std::to_string(monkeys["root"]->evaluate());
+    }
+
+    string answer_b(vector<string> const& inputData)
     {
         return "PENDING";
     }
--- 2022-cpp/aoc2022/tests/day-21-test.cpp
+++ 2022-cpp/aoc2022/tests/day-21-test.cpp
@@ -2,18 +2,30 @@
 
 #include <days.h>
 
+std::vector<std::string> const data{
+    "root: pppw + sjmn",
+    "dbpl: 5",
+    "cczh: sllz + lgvd",
+    "zczc: 2",
+    "ptdq: humn - dvpt",
+    "dvpt: 3",
+    "lfqf: 4",
+    "humn: 5",
+    "ljgn: 2",
+    "sjmn: drzm * dbpl",
+    "sllz: 4",
+    "pppw: cczh / lfqf",
+    "lgvd: ljgn * ptdq",
+    "drzm: hmdt - zczc",
+    "hmdt: 32",
+};
+
 TEST(Day21, Part1Example)
 {
-    std::vector<std::string> data{
-    };
-
-    EXPECT_EQ("PENDING", day_21::answer_a(data));
+    EXPECT_EQ("152", day_21::answer_a(data));
 }
 
 TEST(Day21, Part2Example)
 {
-    std::vector<std::string> data{
-    };
-
     EXPECT_EQ("PENDING", day_21::answer_b(data));
 }
