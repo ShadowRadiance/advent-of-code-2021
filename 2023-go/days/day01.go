8d7ee9d732d89e05952bd7be9d3867def43cac0e
Megan McVey <shadowradiance@me.com>
2023-12-07 15:00:14 -0400

    Day 1 Part 1

--- 2023-go/days/day01.go
+++ 2023-go/days/day01.go
@@ -1,6 +1,31 @@
 package days
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Day01 struct{}
 
-func (Day01) Part01(_ string) string { return "PENDING" }
+func (Day01) Part01(input string) string {
+	lines := strings.Split(input, "\n")
+	const numberChars = "1234567890"
+	numbers := make([]int, 0, len(lines))
+
+	for _, line := range lines {
+		firstIdx := strings.IndexAny(line, numberChars)
+		lastIdx := strings.LastIndexAny(line, numberChars)
+		numberStr := string(line[firstIdx]) + string(line[lastIdx])
+		number, _ := strconv.Atoi(numberStr)
+		numbers = append(numbers, number)
+	}
+
+	sum := 0
+	for _, number := range numbers {
+		sum = sum + number
+	}
+
+	return strconv.Itoa(sum)
+}
+
 func (Day01) Part02(_ string) string { return "PENDING" }
