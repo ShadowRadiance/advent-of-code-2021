8ea5605566fe4b4d88c5b0c3136abc1cd92a03c8
Megan McVey <shadowradiance@me.com>
2023-12-10 19:11:44 -0400

    Refactor Day 03

--- 2023-go/days/day03/day03.go
+++ 2023-go/days/day03/day03.go
@@ -16,21 +16,36 @@ func (g Grid) at(x, y int) rune {
 func (g Grid) numRows() int { return len(g) }
 func (g Grid) numCols() int { return len(g[0]) }
 
-func (Solution) Part01(input string) string {
-	lines := strings.Split(input, "\n")
-	if len(lines) == 0 {
-		return "NO DATA"
-	}
+type Part struct {
+	x1, x2, y int
+	number    int
+}
 
-	var grid Grid = make(Grid, 0, len(lines))
+type Gear struct {
+	x, y  int
+	parts []*Part
+}
+
+func (g Gear) ratio(total int) {
+	total = 1
+	for _, p := range g.parts {
+		total *= p.number
+	}
+	return
+}
+
+func buildGrid(lines []string) (grid Grid) {
+	grid = make(Grid, 0, len(lines))
 	for _, line := range lines {
 		if len(line) != 0 {
 			grid = append(grid, line)
 		}
 	}
+	return
+}
 
-	collectedPartNumbers := make([]string, 0)
-	sumOfPartNumbers := 0
+func collectParts(grid Grid) (collectedParts []Part) {
+	collectedParts = make([]Part, 0)
 	for r := 0; r < grid.numRows(); r++ {
 		for c := 0; c < grid.numCols(); c++ {
 			char := grid.at(c, r)
@@ -47,21 +62,19 @@ func (Solution) Part01(input string) string {
 			}
 
 			if len(numberStr) != 0 {
+				number := util.ConvertNumeric(numberStr)
 				// numberStr runs from grid[r][c-len(numberStr)+1] to grid[r][c]
-				if _, found := grid.findSymbolAround(c-len(numberStr), r, c-1, r); found {
-					collectedPartNumbers = append(collectedPartNumbers, numberStr)
-					sumOfPartNumbers += util.ConvertNumeric(numberStr)
+				potentialPart := Part{c - len(numberStr), c - 1, r, number}
+				if _, found := grid.findSymbolAround(potentialPart); found {
+					collectedParts = append(collectedParts, potentialPart)
 				}
 			}
 		}
 	}
-
-	//fmt.Println(collectedPartNumbers)
-
-	return strconv.Itoa(sumOfPartNumbers)
+	return
 }
 
-func (g Grid) findSymbolAround(x1, y1, x2, y2 int) (rune, bool) {
+func (g Grid) findSymbolAround(part Part) (rune, bool) {
 	// we need to look around that location for a symbol
 	//
 	// ???????????
@@ -71,20 +84,38 @@ func (g Grid) findSymbolAround(x1, y1, x2, y2 int) (rune, bool) {
 	// note that we cannot look off the edges of the grid
 
 	// push out the boundaries, respect the edges
-	x1 = util.MaxInt(x1-1, 0)
-	y1 = util.MaxInt(y1-1, 0)
-	x2 = util.MinInt(x2+1, g.numCols()-1)
-	y2 = util.MinInt(y2+1, g.numRows()-1)
+	lft := util.MaxInt(part.x1-1, 0)
+	top := util.MaxInt(part.y-1, 0)
+	rgt := util.MinInt(part.x2+1, g.numCols()-1)
+	bot := util.MinInt(part.y+1, g.numRows()-1)
 
-	for x := x1; x <= x2; x++ {
-		for y := y1; y <= y2; y++ {
-			char := g.at(x, y)
-			if !strings.ContainsRune("0123456789.", char) {
-				return char, true
+	for x := lft; x <= rgt; x++ {
+		for y := top; y <= bot; y++ {
+			if x < part.x1 || x > part.x2 || y != part.y { // don't check inside the number
+				char := g.at(x, y)
+				if !strings.ContainsRune("0123456789.", char) {
+					return char, true
+				}
 			}
 		}
 	}
 	return 0, false
+}
+
+func (Solution) Part01(input string) string {
+	lines := strings.Split(input, "\n")
+	if len(lines) == 0 {
+		return "NO DATA"
+	}
+	grid := buildGrid(lines)
+	collectedParts := collectParts(grid)
+
+	sumOfPartNumbers := 0
+	for _, part := range collectedParts {
+		sumOfPartNumbers += part.number
+	}
+
+	return strconv.Itoa(sumOfPartNumbers)
 }
 
 func (Solution) Part02(input string) string {
