e69524bec62a11d969d0c8cb746cb582d195454d
Megan McVey <shadowradiance@me.com>
2023-12-15 13:14:49 -0400

    Day 10 Part 1 Complete

--- 2023-go/days/day10/solution.go
+++ 2023-go/days/day10/solution.go
@@ -1,7 +1,10 @@
 package day10
 
 import (
+	"strconv"
 	"strings"
+
+	"github.com/shadowradiance/advent-of-code/2023-go/util"
 )
 
 type Solution struct{}
@@ -24,7 +27,32 @@ func (Solution) Part01(input string) string {
 	// •L-J•  =>  •••••  =>  •2•••  =>  •23••  =>  •234•  =>
 	// •••••  =>  •••••  =>  •••••  =>  •••••  =>  •••••  =>
 
-	return "PENDING"
+	// the description makes it sound like pathfinding, but there is only one path
+	// so lets follow it until we get back to the start and divide by two to get the "furthest steps away"
+
+	grid := makeGrid(lines)
+	start := findStart(grid)
+	rover := Rover{start, allowableDirectionFrom(grid, start)}
+	// displayGrid(grid, rover)
+
+	moves := 0
+	for {
+		instruction := grid.atPos(rover.pos)
+		if instruction == 'S' {
+			if moves != 0 {
+				break
+			} else {
+				rover.move(1)
+				moves++
+			}
+		} else {
+			rover.processInstruction(instruction)
+			moves++
+		}
+	}
+	// displayGrid(grid, rover)
+
+	return strconv.Itoa(moves / 2)
 }
 
 func (Solution) Part02(input string) string {
@@ -34,4 +62,180 @@ func (Solution) Part02(input string) string {
 	}
 
 	return "PENDING"
+}
+
+type Grid [][]rune
+
+func (g Grid) at(x, y int) rune        { return g[y][x] }
+func (g Grid) atPos(pos Position) rune { return g.at(pos.x, pos.y) }
+func (g Grid) height() int             { return len(g) }
+func (g Grid) width() int              { return len(g[0]) }
+
+type Position struct {
+	x, y int
+}
+
+type Direction Position
+
+var (
+	North = Direction{0, -1}
+	South = Direction{0, 1}
+	West  = Direction{-1, 0}
+	East  = Direction{1, 0}
+)
+
+type Rover struct {
+	pos    Position
+	facing Direction
+}
+
+func (pos Position) Add(dir Direction) Position {
+	return Position{x: pos.x + dir.x, y: pos.y + dir.y}
+}
+
+func (pos Position) InBounds(x1, y1, x2, y2 int) bool {
+	return pos.x >= x1 && pos.x <= x2 && pos.y >= y1 && pos.y <= y2
+}
+
+func (pos Position) OutOfBounds(x1, y1, x2, y2 int) bool {
+	return !pos.InBounds(x1, y1, x2, y2)
+}
+
+func (dir Direction) ScalarProduct(n int) Direction {
+	return Direction{x: dir.x * n, y: dir.y * n}
+}
+
+func (dir Direction) Reverse() Direction {
+	return dir.ScalarProduct(-1)
+}
+
+func (rover *Rover) processInstruction(instruction rune) {
+	switch instruction {
+	case '|': // do nothing
+	case '-': // do nothing
+	case 'L':
+		if rover.facing == South {
+			rover.facing = East
+		} else if rover.facing == West {
+			rover.facing = North
+		}
+	case 'J':
+		if rover.facing == South {
+			rover.facing = West
+		} else if rover.facing == East {
+			rover.facing = North
+		}
+	case '7':
+		if rover.facing == North {
+			rover.facing = West
+		} else if rover.facing == East {
+			rover.facing = South
+		}
+	case 'F':
+		if rover.facing == North {
+			rover.facing = East
+		} else if rover.facing == West {
+			rover.facing = South
+		}
+	case '.':
+		panic("Escaped the pipe somehow")
+	case 'S':
+		panic("Caller didn't resolve 'S' before passing")
+	default:
+		panic("Unknown letter WTF: " + string(instruction))
+	}
+	rover.move(1)
+}
+
+func (rover *Rover) move(n int) {
+	rover.pos = rover.pos.Add(rover.facing.ScalarProduct(n))
+}
+
+func (rover *Rover) arrow() rune {
+	switch rover.facing {
+	case North:
+		return '↑'
+	case South:
+		return '↓'
+	case East:
+		return '→'
+	case West:
+		return '←'
+	default:
+		return '•'
+	}
+}
+
+func findStart(grid Grid) Position {
+	for y := 0; y < grid.height(); y++ {
+		for x := 0; x < grid.width(); x++ {
+			if grid.at(x, y) == 'S' {
+				return Position{x, y}
+			}
+		}
+	}
+	panic("Start not found!")
+}
+
+func allowableDirectionFrom(grid Grid, start Position) Direction {
+	var newPos Position
+	for _, direction := range []Direction{North, East, South, West} {
+		newPos = start.Add(direction)
+		if newPos.InBounds(0, 0, grid.width()-1, grid.height()-1) {
+			instruction := grid.atPos(newPos)
+			if connected(instruction, direction.Reverse()) {
+				return direction
+			}
+		}
+	}
+	panic("No allowable directions from position!")
+}
+
+func connected(instruction rune, direction Direction) bool {
+	switch instruction {
+	case '|':
+		return direction == North || direction == South
+	case '-':
+		return direction == West || direction == East
+	case 'L':
+		return direction == North || direction == East
+	case 'J':
+		return direction == North || direction == West
+	case '7':
+		return direction == South || direction == West
+	case 'F':
+		return direction == South || direction == East
+	case '.':
+		return false
+	case 'S':
+		panic("cannot read connected from S")
+	default:
+		panic("Unknown letter WTF")
+	}
+}
+
+func displayGrid(grid Grid, rover Rover) {
+	grid2 := make([][]rune, grid.height())
+	for row, runeLine := range grid {
+		grid2[row] = make([]rune, grid.width())
+		for col, runeChar := range runeLine {
+			grid2[row][col] = runeChar
+		}
+	}
+	grid2[rover.pos.y][rover.pos.x] = rover.arrow()
+
+	for _, s := range grid2 {
+		println(string(s))
+	}
+	println(strings.Repeat("-", grid.width()))
+}
+
+func makeGrid(lines []string) Grid {
+	lines = util.Filter(lines, func(s string) bool { return len(s) > 0 })
+	runeLines := make([][]rune, len(lines))
+
+	for i, line := range util.Filter(lines, func(s string) bool { return len(s) > 0 }) {
+		runeLines[i] = []rune(line)
+	}
+	return runeLines
 }
--- 2023-go/days/day10/solution_test.go
+++ 2023-go/days/day10/solution_test.go
@@ -22,6 +22,16 @@ func TestSolution_Part01(t *testing.T) {
 		},
 		{
 			Input: heredoc.Doc(`
+				-L|-F7
+				7S--7|
+				L|7-||
+				-L--J|
+				L|--JF
+			`),
+			Expected: "5",
+		},
+		{
+			Input: heredoc.Doc(`
 				7-F7-
 				.FJ|7
 				SJLL7
@@ -41,11 +51,68 @@ func TestSolution_Part01(t *testing.T) {
 func TestSolution_Part02(t *testing.T) {
 	examples := []util.TestExample{
 		{
-			Input:    heredoc.Doc(``),
-			Expected: "PENDING",
+			Input: heredoc.Doc(`
+				...........
+				.S-------7.
+				.|F-----7|.
+				.||.....||.
+				.||.....||.
+				.|L-7.F-J|.
+				.|..|.|..|.
+				.L--J.L--J.
+				...........
+			`),
+			Expected: "4",
+		},
+		{
+			Input: heredoc.Doc(`
+				..........
+				.S------7.
+				.|F----7|.
+				.||....||.
+				.||....||.
+				.|L-7F-J|.
+				.|II||II|.
+				.L--JL--J.
+				..........
+			`),
+			Expected: "4",
+		},
+		{
+			Input: heredoc.Doc(`
+				.F----7F7F7F7F-7....
+				.|F--7||||||||FJ....
+				.||.FJ||||||||L7....
+				FJL7L7LJLJ||LJ.L-7..
+				L--J.L7...LJS7F-7L7.
+				....F-J..F7FJ|L7L7L7
+				....L7.F7||L7|.L7L7|
+				.....|FJLJ|FJ|F7|.LJ
+				....FJL-7.||.||||...
+				....L---J.LJ.LJLJ...
+			`),
+			Expected: "8",
+		},
+		{
+			Input: heredoc.Doc(`
+				FF7FSF7F7F7F7F7F---7
+				L|LJ||||||||||||F--J
+				FL-7LJLJ||||||LJL-77
+				F--JF--7||LJLJ7F7FJ-
+				L---JF-JLJ.||-FJLJJ7
+				|F|F-JF---7F7-L7L|7|
+				|FFJF7L7F-JF7|JL---7
+				7-L-JL7||F7|L7F-7F7|
+				L.L7LFJ|||||FJL7||LJ
+				L7JLJL-JLJLJL--JLJ.L
+			`),
+			Expected: "10",
 		},
 	}
-	for _, example := range examples {
+	for i, example := range examples {
+		if i == 100 {
+			break
+		}
 		actual := Solution{}.Part02(example.Input)
 		if example.Expected != actual {
 			t.Errorf("Expected: %v, got: %v", example.Expected, actual)
