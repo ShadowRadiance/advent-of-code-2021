913d08164727ed8494805d7738794f7b93e41e94
Megan McVey <shadowradiance@me.com>
2024-10-22 10:17:19 -0300

    Day 04 Complete

--- 2015-csharp/AOC2015.Tests/Day04.cs
+++ 2015-csharp/AOC2015.Tests/Day04.cs
@@ -6,7 +6,8 @@ namespace AOC2015.Tests
     public class Day04Test
     {
         [Theory]
-        [InlineData(new string[] { "" }, "")]
+        [InlineData(new string[] { "abcdef" }, "609043")]
+        [InlineData(new string[] { "pqrstuv" }, "1048970")]
         public void PartA(string[] data, string expectation)
         {
             Assert.Equal(expectation, new Day04(data).PartA());
@@ -14,7 +15,7 @@ namespace AOC2015.Tests
 
 
         [Theory]
-        [InlineData(new string[] { "" }, "")]
+        [InlineData(new string[] { "abcdef" }, "6742839")]
         public void PartB(string[] data, string expectation)
         {
             Assert.Equal(expectation, new Day04(data).PartB());
--- 2015-csharp/AOC2015/Days/Day04.cs
+++ 2015-csharp/AOC2015/Days/Day04.cs
@@ -1,16 +1,57 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace AOC2015.Days;
 
 public class Day04 : Day
 {
     public Day04(string[] data) : base(data) { }
 
+    private static IEnumerable<Tuple<long, string>> MD5Generator(string secret_key)
+    {
+        long index = 1;
+        while (index < long.MaxValue)
+        {
+            string input = $"{secret_key}{index}";
+            yield return new Tuple<long, string>(
+                index,
+                Convert.ToHexString(MD5.HashData(Encoding.UTF8.GetBytes(input)))
+            );
+            index += 1;
+        }
+    }
+
     public override string PartA()
     {
-        return "";
+        // find MD5 hashes which, in hexadecimal, start with at least five zeroes.
+        // The input to the MD5 hash is some secret key (your puzzle input, given below) followed by a number in decimal.
+        // find Santa the lowest positive number (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+
+        string secret_key = Data[0];
+
+        foreach (var result in MD5Generator(secret_key))
+        {
+            if (result.Item2.StartsWith("00000"))
+            {
+                return result.Item1.ToString();
+            }
+        }
+
+        return "NOT FOUND";
     }
 
     public override string PartB()
     {
-        return "";
+        string secret_key = Data[0];
+
+        foreach (var result in MD5Generator(secret_key))
+        {
+            if (result.Item2.StartsWith("000000"))
+            {
+                return result.Item1.ToString();
+            }
+        }
+
+        return "NOT FOUND";
     }
 }
