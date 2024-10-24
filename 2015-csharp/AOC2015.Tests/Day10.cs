3548bf0d85612fe1c98852e171df520f54b15960
Megan McVey <shadowradiance@me.com>
2024-10-24 11:16:08 -0300

    Day 10 Complete

--- 2015-csharp/AOC2015.Tests/Day10.cs
+++ 2015-csharp/AOC2015.Tests/Day10.cs
@@ -1,4 +1,3 @@
-using Xunit;
 using AOC2015.Days;
 
 namespace AOC2015.Tests
@@ -6,22 +5,25 @@ namespace AOC2015.Tests
     public class Day10Test
     {
         [Theory]
-        [InlineData(new string[] { "1" }, 1, "11")]
-        [InlineData(new string[] { "11" }, 1, "21")]
-        [InlineData(new string[] { "21" }, 1, "1211")]
-        [InlineData(new string[] { "1211" }, 1, "111221")]
-        [InlineData(new string[] { "111221" }, 1, "312211")]
+        [InlineData("1", "11")]
+        [InlineData("11", "21")]
+        [InlineData("21", "1211")]
+        [InlineData("1211", "111221")]
+        [InlineData("111221", "312211")]
+        public void PlayLookAndSay_Test(string input, string expectation)
+        {
+            Assert.Equal(expectation, Day10.PlayLookAndSay(input));
+        }
+
+        [Theory]
+        [InlineData(new string[] { "1" }, 1, "2")]
+        [InlineData(new string[] { "11" }, 1, "2")]
+        [InlineData(new string[] { "21" }, 1, "4")]
+        [InlineData(new string[] { "1211" }, 1, "6")]
+        [InlineData(new string[] { "111221" }, 1, "6")]
         public void PartA(string[] data, int iterations, string expectation)
         {
             Assert.Equal(expectation, new Day10(data, iterations).PartA());
-        }
-
-
-        [Theory]
-        [InlineData(new string[] { "" }, 1, "")]
-        public void PartB(string[] data, int iterations, string expectation)
-        {
-            Assert.Equal(expectation, new Day10(data, iterations).PartB());
         }
     }
 }
--- 2015-csharp/AOC2015/Days/Day10.cs
+++ 2015-csharp/AOC2015/Days/Day10.cs
@@ -2,22 +2,45 @@ namespace AOC2015.Days;
 
 public class Day10 : Day
 {
-    private int _iterations;
+    private int? _iterations;
 
-    public Day10(string[] data, int iterations = 40) : base(data)
+    // standard constructor
+    public Day10(string[] data) : base(data) => _iterations = null;
+    // constructor for tests
+    public Day10(string[] data, int iterations) : base(data) => _iterations = iterations;
+
+    private struct DigitRun(char digit, int count)
     {
-        _iterations = iterations;
+        public char digit = digit;
+        public int count = count;
     }
 
-    private string PlayLookAndSay(string input)
+    public static string PlayLookAndSay(string input)
     {
+        if (input == "") return "";
+
         // break input into runs of repeated digits
-        // replace each run with run.length run.digit
+        List<DigitRun> runs = [];
+        DigitRun currentRun = new(digit: input[0], count: 1);
+        for (int index = 1; index < input.Length; index++)
+        {
+            if (input[index] == currentRun.digit)
+            {
+                currentRun.count += 1;
+            }
+            else
+            {
+                runs.Add(currentRun);
+                currentRun = new(digit: input[index], count: 1);
+            }
+        }
+        runs.Add(currentRun);
 
-        return input;
+        // replace each run with run.length run.digit
+        return string.Join("", runs.Select(run => $"{run.count}{run.digit}"));
     }
 
-    public override string PartA()
+    private string Solution()
     {
         string input = Data[0];
         for (int i = 0; i < _iterations; i++)
@@ -27,8 +50,16 @@ public class Day10 : Day
         return input.Length.ToString();
     }
 
+    public override string PartA()
+    {
+        if (_iterations == null) _iterations = 40;
+        return Solution();
+    }
+
     public override string PartB()
     {
-        return "";
+        if (_iterations == null) _iterations = 50;
+
+        return Solution();
     }
 }
